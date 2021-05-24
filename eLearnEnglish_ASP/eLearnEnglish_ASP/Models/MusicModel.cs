a85e4216c0c32652ebc56c75b24c0415a5ad3415
Reckle <50696668+Reckle@users.noreply.github.com>
2021-05-24 12:17:22 +0300

    2-try

--- eLearnEnglish_ASP/eLearnEnglish_ASP/Models/MusicModel.cs
+++ eLearnEnglish_ASP/eLearnEnglish_ASP/Models/MusicModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,21 @@ namespace eLearnEnglish_ASP.Models
 {
     public class MusicModel
     {
+        public int Id { get; set; }
+        [StringLength(100, MinimumLength = 5)]
+        [Required(ErrorMessage = "Please enter the name of the song")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter the author name")]
+        public string Author { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        [Display(Name = "Choose the cover photo of your book")]
+        [Required]
+       // public IFormFile CoverPhoto { get; set; }
+        public string CoverImageUrl { get; set; }
+
+        public string MusicUrl { get; set; }
     }
 }
